f10c6dbcb60159154dc80d242ac0c3e61901e443
S. Mobin Arablou <mobinlion@yahoo.com>
2018-05-10 22:25:22 +0800

    Added 2018 and 2019 years, changed svg from 2k to infinity, added container for years

---
+++ assets/js/teslamobin.js
@@ -0,0 +1,18 @@
+	
+	$("#btn2k18").click(function(){
+		$("#2019").fadeOut(0);
+		$("#2018").fadeIn(400);
+		
+	});
+
+	$("#btn2k19").click(function(){
+		$("#2018").fadeOut(0);
+		$("#2019").fadeIn(400);
+		
+	});
+	
+	
+	
+	
+
+
